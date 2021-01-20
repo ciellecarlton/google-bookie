691822e44fe31003f72b020235e153ad5b872d92
Cielle Carlton <cielle.carlton13@gmail.com>
2021-01-20 13:54:03 -0500

    added proc file

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -9,21 +8,27 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-
-app.use(routes);
-
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  process.env.MONGODB_URI || "mongodb://localhost/google-bookie",
   {
     useCreateIndex: true,
     useNewUrlParser: true
   }
 );
+
+app.use(routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+
 
 
 app.listen(PORT, () =>
